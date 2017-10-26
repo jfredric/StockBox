5c9a0aaf2f8d8fe8bdbad1cdd361f6ec53257a83
jfredrickson <jfredric@gmail.com>
2017-10-25 19:19:43 -0700

    forgot to added the needed extension

--- StockBox/Utilities/Utility.swift
+++ StockBox/Utilities/Utility.swift
@@ -17,6 +17,27 @@ func stringTrimmer(stringToTrim string: String?) -> String? {
 
 // MARK: ALERTS
 
+// allows you to get the topmost view at any current time
+extension UIApplication {
+    
+    static func topViewController(base: UIViewController? = UIApplication.shared.delegate?.window??.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return topViewController(base: selected)
+        }
+        
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        
+        return base
+    }
+}
+
 // send user a message
 func messageAlert(title: String, message: String?, from: UIViewController?) {
     
