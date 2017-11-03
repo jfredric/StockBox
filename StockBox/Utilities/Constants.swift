2d9b6401e3cfd3a962073dd0314ba19bc7435818
Jareedos <jaredallens@gmail.com>
2017-11-02 21:44:34 -0700

    Created Constants file & added constant for our Orange Color

---
+++ StockBox/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockBox
+//
+//  Created by Jared Sobol on 11/2/17.
+//  Copyright © 2017 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+let MAINORANGECOLOR = UIColor(red:0.93, green:0.34, blue:0.12, alpha:1.0)
