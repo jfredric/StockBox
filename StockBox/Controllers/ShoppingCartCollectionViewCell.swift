01fcaf331be5405dff9b1fdfdcf1d3df2596cdc3
Jareedos <jaredallens@gmail.com>
2017-11-07 10:26:20 -0800

    Implemented shoppingcart CollectionView

--- StockBox/Controllers/ShoppingCartCollectionViewCell.swift
+++ StockBox/Controllers/ShoppingCartCollectionViewCell.swift
@@ -18,9 +18,20 @@ class ShoppingCartCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var numOfProducts: UILabel!
     
     @IBOutlet weak var productPrice: UILabel!
+    
+    @IBOutlet weak var totalLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func deleteBtnPressed(_ sender: Any) {
+    }
+    override func layoutSubviews() {
+        layer.shadowRadius = 8
+        layer.shadowOpacity = 0.3
+        layer.shadowOffset = CGSize(width: 5, height: 5)
+        layer.cornerRadius = BUTTONCORNERRADIUS
+        self.clipsToBounds = false
+    }
 }
