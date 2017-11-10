68e5a66314706dc8d2041843c98945b73551376c
Jareedos <jaredallens@gmail.com>
2017-11-10 11:35:28 -0800

    added Unit tests

--- StockBoxTests/StockBoxTests.swift
+++ StockBoxTests/StockBoxTests.swift
@@ -22,9 +22,29 @@ class StockBoxTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testStringTimmer() {
+        let string1 = " HEY I JUST MEET YOU "
+        let result2 = "HEY I JUST MEET YOU"
+        let result1 = stringTrimmer(stringToTrim: string1)
+        XCTAssertEqual(result1, result2)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testDoubleToCurrency() {
+        let doubleValue = 9.0
+        let result1 = "$9.00"
+        let result2 = doubleToCurrencyString(value: doubleValue)
+        XCTAssertEqual(result1, result2)
+    }
+    
+    func testTrimmedAndUnWrapped() {
+        let string1 = "Hey"
+        let string2 = "WhatsUP"
+        let result1 = ("Hey", "WhatsUP")
+        let result2 = trimmedAndUnwrappedTextFieldInputs(email: string1, password: string2)
+        let tested = result1.0 == result2.0 && result1.1 == result2.1
+        XCTAssertEqual(tested,true)
     }
     
     func testPerformanceExample() {
